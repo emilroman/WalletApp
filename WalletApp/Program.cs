4827641361eaf56104537198fd4c3e90759fd377
emilroman <emir23us@yahoo.com>
2018-03-24 14:02:27 +0200

    Add project files.

---
+++ WalletApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WalletApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
